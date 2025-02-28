2c689332a669b2394b5b61b710c8992c3f59b161
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2025-02-28 09:25:09 -0800

    sky:Bringup AxionOS

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common pixelage Os configurations
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+# Inherit common lineage Os configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/pixelage/config/BoardConfigReservedSize.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -23,11 +23,15 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
-PIXELAGE_MAINTAINER := Dhanush
+# Axion Stuff
+AXION_CAMERA_REAR_INFO := 50,2
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := Dhanush
+AXION_PROCESSOR := Snapdragon®_4_Gen2
 
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := pixelage_sky
+PRODUCT_NAME := lineage_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
